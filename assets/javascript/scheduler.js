40916d7618ef91b5f66006e31c1e87026efa2c80
ryanosaurus-tex <rmw.inbox@gmail.com>
2017-09-14 17:10:04 -0500

    now its not working

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -1,13 +1,14 @@
  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAUlH7bq1EM7kkX9kEQsn_8nnBkUehPo0Q",
-    authDomain: "train-scheduler-774ac.firebaseapp.com",
-    databaseURL: "https://train-scheduler-774ac.firebaseio.com",
-    projectId: "train-scheduler-774ac",
-    storageBucket: "train-scheduler-774ac.appspot.com",
-    messagingSenderId: "188867108749"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyAUlH7bq1EM7kkX9kEQsn_8nnBkUehPo0Q",
+  authDomain: "train-scheduler-774ac.firebaseapp.com",
+  databaseURL: "https://train-scheduler-774ac.firebaseio.com",
+  projectId: "train-scheduler-774ac",
+  storageBucket: "train-scheduler-774ac.appspot.com",
+  messagingSenderId: "188867108749"
+};
+
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -16,7 +17,7 @@ $("#submitButton").on("click", function(event) {
 
 	var trainName = $("#inputTrainName").val().trim();
 	var destination = $("#inputDestination").val().trim();
-	var firstTrainTime = $("#inputFirstTrainTime"); // ADD MOMENT.JS
+	var firstTrainTime = moment($("#inputFirstTrainTime"), "HH:MM" );
 	var frequency = $("#inputFrequency").val().trim();
 
 	var newTrain = {
@@ -43,10 +44,10 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
 	console.log(childSnapshot.val());
 
-	var newTrainName = childSnapshot.val().newTrainName;
-	var newDestination = childSnapshot.val().newDestination;
-	var newFirstTrainTime = childSnapshot.val().newFirstTrainTime;
-	var newFrequency = childSnapshot.val().newFrequency;
+	var currTrainName = childSnapshot.val().newTrainName;
+	var currDestination = childSnapshot.val().newDestination;
+	var currFirstTrainTime = childSnapshot.val().newFirstTrainTime;
+	var currFrequency = childSnapshot.val().newFrequency;
 
 	console.log(newTrainName);
 	console.log(newDestination);
@@ -54,63 +55,9 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	console.log(newFrequency);
 
 	$("#trainTable").append("<tr>" +
-								"<td>" + newTrainName + "</td>" +
-		                        "<td>" + newDestination + "</td>" +
-		                        "<td>" + newFirstTrainTime + "</td>" +
-		                        "<td>" + newFrequency + "</td>" + 
+								"<td>" + currTrainName + "</td>" +
+		                        "<td>" + currDestination + "</td>" +
+		                        "<td>" + currFirstTrainTime + "</td>" +
+		                        "<td>" + currFrequency + "</td>" + 
 		                    "</tr>");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* ------- REFERENCE CODE ----------------------------------
-  
-
-// Create an Object Constructor for the Train listings
-function trainListing(trainName,destination,firstTrainTime,nextTrainTime,frequency) {
-	this.trainName = trainName;
-	this.destination = destination;
-	this.firstTrainTime = firstTrainTime;
-	this.nextTrainTime = nextTrainTime;
-	this.frequency = frequency;
-}
-var database = firebase.database();
-
-
-function addEmployeeListing(){
-
-      // Grabbed values from text boxes
-      name = $("#employeeNameInput").val().trim();
-      role = $("#roleInput").val().trim();
-      startDate = $("#startDateInput").val().trim();
-      monthlyRate = $("#monthlyRateInput").val().trim();
-
-      // Code for handling the push
-      database.ref().push({
-        name: name,
-        role: role,
-        startDate: startDate,
-        monthlyRate: monthlyRate,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-        	});
-};
-*/
